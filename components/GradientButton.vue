7a0058c4a2287adb9bc1c725889c002f113956a5
Anthony Zhen <anthony20151128@gmail.com>
2022-02-14 20:10:53 -0500

    initial

---
+++ components/GradientButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="text-white" onclick="Function">text</button>
+</template>
+
+<script>
+
+export default {
+    name: 'GradientButton',
+    props:{
+        text: String,
+        callback: Function,
+    }
+}
+
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex items-center bg-black justify-between">
+        <h1 class="text-white text-5xl">Susseum</h1>
+        <ul class="list-none">
+            <a href="." class="p-1 inline text-white">Demo</a>
+            <a href="." class="p-1 inline text-white">About</a>
+            <a href="." class="p-1 inline text-white">Solutions</a>
+        </ul>
+    </div>
+</template>
+
+<script>
+import GradientButton from '../components/GradientButton.vue'
+
+export default {
+    name: "Navbar",
+    components: {
+        GradientButton
+    }
+}
+</script>
