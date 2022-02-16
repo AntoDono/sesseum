3fd634c4314b2c271ff085f1f490611d49b08c97
AntoDono <anthony20151128@gmail.com>
2022-02-15 22:25:42 -0500

    added amogus

--- components/Displays/Amongus.vue
+++ components/Displays/Amongus.vue
@@ -5,6 +5,8 @@
 <script>
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 export default {
     name: "Amongus",
@@ -18,20 +20,85 @@ export default {
                 sizes: {
                     width: window.innerWidth/2,
                     height: window.innerHeight
+                },
+                lights:{
+                    spotLight: null,
+                    ambientLight: null,
                 }
+            },
+            loaders:{
+                gltfLoader: null,
+                dracoLoader: null,
+                loadingManager: null,
             }
         }
     },
     methods:{
+        addWall(
+            {
+                length=10, width=10, color='#444444', 
+                metalnaess=0,roughness=0, normalMap=null, 
+                map=null, position={x:0,y:0,z:0}, rotation={x:0,y:0,z:0}
+            } = {}
+        ){
+            const wall = new THREE.Mesh(
+                new THREE.PlaneGeometry(width, length),
+                new THREE.MeshStandardMaterial({
+                    color: color,
+                    metalness: metalnaess,
+                    roughness: roughness,
+                    normalMap: normalMap,
+                    map: map
+                })
+            )
+
+            if (normalMap) wall.normalMap = normalMap
+            if (map) wall.map = map
+
+            wall.receiveShadow = true
+            wall.rotation.set(rotation.x || 0, rotation.y || 0, rotation.z || 0)
+            wall.position.set(position.x || 0, position.y || 0, position.z || 0)
+            this.graphics.scene.add(wall)
+        },
+        load_model(path, propname, coords={x:0,y:0,z:0}, rotation={x:0, y:0, z:0}, scale={x:1, y:1, z:1} ){
+            this.loaders.gltfLoader.load(
+                path,
+                (gltf) =>
+                {
+                    let prop = gltf.scene
+
+                    prop.name = propname
+
+                    prop.position.x = coords.x || 0
+                    prop.position.y = coords.y || 0
+                    prop.position.z = coords.z || 0
+                    
+                    prop.rotation.x = rotation.x || 0
+                    prop.rotation.y = rotation.y || 0
+                    prop.rotation.z = rotation.z || 0
+
+                    prop.scale.x = scale.x || 1
+                    prop.scale.y = scale.y || 1
+                    prop.scale.z = scale.z || 1
+
+                    prop.receiveShadow = true
+
+                    this.graphics.scene.add(prop)
+
+                    console.log(prop)
+
+                }
+            )
+
+        },
         tick(){
             this.graphics.renderer.render(this.graphics.scene, this.graphics.camera)
+            // this.graphics.controls.update()
             window.requestAnimationFrame(this.tick)
-
         },
         init(){
 
-            console.log(this.graphics.sizes)
-
+        
             const canvas = document.querySelector('canvas.webgl')
             this.graphics.renderer = new THREE.WebGLRenderer({
                 canvas: canvas
@@ -40,13 +107,17 @@ export default {
             this.graphics.camera = new THREE.PerspectiveCamera(75, this.graphics.sizes.width / this.graphics.sizes.height, 0.1, 100)
             this.graphics.scene.add(this.graphics.camera)
 
-            this.graphics.controls = new OrbitControls( this.graphics.camera, this.graphics.renderer );
+            this.graphics.controls = new OrbitControls( this.graphics.camera, canvas );
+            this.graphics.controls.enableDamping = true
 
-            const geometry = new THREE.BoxGeometry();
-            const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-            const cube = new THREE.Mesh( geometry, material );
-            cube.rotation.set(1,1,1)
-            this.graphics.scene.add( cube );
+            this.graphics.camera.position.set(0,3.5,5)
+            this.graphics.camera.rotation.set(-Math.PI/8,0,0)
+
+            // const geometry = new THREE.BoxGeometry();
+            // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+            // const cube = new THREE.Mesh( geometry, material );
+            // cube.rotation.set(1,1,1)
+            // this.graphics.scene.add( cube );
 
             this.graphics.camera.position.z = 5;
 
@@ -65,6 +136,21 @@ export default {
                 this.graphics.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
             })
 
+            this.graphics.lights.spotLight = new THREE.SpotLight( 0xffffff, 5, 100 ); // Adds spot light
+            this.graphics.lights.spotLight.position.set( 0, 10, 0 );
+
+            this.graphics.lights.ambientLight = new THREE.AmbientLight( "#b0b0b0" ); // soft white light
+
+            // Adds floor
+            this.addWall({
+                length: 100,
+                width: 100,
+                rotation: {x: - Math.PI/2},
+            })
+
+            this.graphics.scene.add(this.graphics.lights.spotLight)
+            this.graphics.scene.add(this.graphics.lights.ambientLight)
+
             this.tick()
 
         }
@@ -73,6 +159,25 @@ export default {
 
         this.graphics.scene = new THREE.Scene()
         this.graphics.scene.background = new THREE.Color("gray")
+
+        this.loaders.loadingManager = new THREE.LoadingManager(
+            ()=>{ // Done loading callback
+                console.log("Done loading")
+            },
+            (itemUrl, itemsLoaded, itemsTotal)=>{ // Progress call back
+                let percent = itemsLoaded/itemsTotal
+                console.log(`Current loading progress: ${percent}`)
+                this.$emit('loading', percent)
+            }
+        )
+
+        this.loaders.dracoLoader = new DRACOLoader(this.loaders.loadingManager) 
+        this.loaders.dracoLoader.setDecoderPath('/draco/')
+
+        this.loaders.gltfLoader = new GLTFLoader(this.loaders.loadingManager)
+        this.loaders.gltfLoader.setDRACOLoader(this.loaders.dracoLoader)
+
+        this.load_model('blender/amongus/scene.gltf')
 
         this.init()
     }
--- components/Main.vue
+++ components/Main.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <div class="z-10 relative top-0 right-12 slant skew-x-6 bg-gradient-to-bl from-torange to-tdred h-screen w-7/12">
+        <div class="z-10 relative top-0 right-14 slant skew-x-6 bg-gradient-to-bl from-torange to-tdred h-screen w-7/12">
 
         </div>
         <div class="z-10 absolute top-0 left-0 h-screen w-6/12">
             <div class="relative top-80 left">
-                <h2 class="text-4xl font-redressed text-white">Susseum, where Sus is a lifestyle.</h2>
+                <h2 class="w-10/12 text-4xl font-redressed text-white">Susseum, where Sus is a lifestyle.</h2>
                 <GradientButton class="relative top-8" :text="'View Demo →'"/>
             </div>
         </div>
